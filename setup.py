560ee0295e2f948a40bb82e8933a77e9951b75c4
joran <joran@decagon.com>
2017-05-10 23:57:03 -0700

    deleted build folder

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
-
+import os
+print os.name
+scripts = ['bin/aptly2html','bin/aptly2json'] if os.name != "nt" else ['bin/aptly2html.cmd','bin/aptly2json.cmd']
+print scripts
 setup(
     name='aptly2html',
     version='0.1',
     packages=['aptly2html_data'],
     py_modules=['aptly2html','aptly2json'],
+    scripts=scripts,
     url='',
     license='MIT',
     author='Joran Beasley',
